8d33d3dfb1b069c7d3a1bed78c4d2f0152c09d21
Anusha <anushatest199126@gmail.com>
2025-06-10 22:00:28 -0400

    first comment

---
+++ src/test/java/rahulshettyacademy/DemoTest.java
@@ -0,0 +1,24 @@
+package rahulshettyacademy;
+
+import java.net.URL;
+
+import org.testng.annotations.Test;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+public class DemoTest {
+	@Test
+	public class Test1{
+		 UiAutomator2Options options = new UiAutomator2Options();
+	     //   options.setDeviceName("Anusha");
+	       // options.setApp("C:\\Users\\santh\\Downloads\\Appium\\Appium\\src\\test\\java\\ApiDemos-debug.apk");
+	       // options.setApp("C://Users//santh//Downloads//Appium//Appium//src//test//java//ApiDemos-debug.apk");
+	    //    AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
+
+	        // Add your test logic here
+
+	       // driver.quit(); // Always close the driver
+	}
+
+}
