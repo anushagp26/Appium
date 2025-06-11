5dff6efdb49211aca11e0b3985d3d4e2884ff1c9
Anusha <anushatest199126@gmail.com>
2025-06-11 00:44:47 -0400

    add file

---
+++ src/test/java/rahulshettyacademy/Demo2.java
@@ -0,0 +1,29 @@
+package rahulshettyacademy;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] str= {1,2,3,1};  
+		boolean flag = false;
+		{
+	for(int i=0;i<str.length;i++)
+	{
+	for(int j=i+1;j<str.length;j++)
+	{
+	if(str[i]==str[j])
+	{
+	System.out.println(str[i]);
+	flag = true;
+	}
+	}
+	}
+	if(flag ==false)
+	{
+	    System.out.println("not");
+	}
+		}
+		
+	}
+
+}
