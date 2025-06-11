ff4b896d4d5fc5f42ab503ff85b8edcbf5fcb7a1
Anusha <anushatest199126@gmail.com>
2025-06-10 22:56:10 -0400

    Demo2

--- src/test/java/rahulshettyacademy/DemoTest.java
+++ src/test/java/rahulshettyacademy/DemoTest.java
@@ -19,6 +19,28 @@ public class DemoTest {
 	        // Add your test logic here
 
 	       // driver.quit(); // Always close the driver
+		  int[] str= {1,2,3,1};  
+			boolean flag = false;
+			{
+		for(int i=0;i<str.length;i++)
+		{
+		for(int j=i+1;j<str.length;j++)
+		{
+		if(str[i]==str[j])
+		{
+		System.out.println(str[i]);
+		flag = true;
+		}
+		}
+		}
+		if(flag ==false)
+		{
+		    System.out.println("not");
+		}
+		    
+		 
 	}
 
 }
+}
+
